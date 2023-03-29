e12b46453fd5c1be2be2fa542751dbbc1233e242
kwetso12 <kgaladikwetso@gmail.com>
2023-03-29 16:07:34 +0200

    fixed all errors in all challenges (IWA3)

---
+++ IWA3/challenge_2/alex.js
@@ -0,0 +1,8 @@
+// alex.js
+
+const firstname = "Alex"
+const surname = "Naidoo"
+export const role3 = "head of marketing"
+
+const display= firstname + " " + surname + " (" + role3 + ")";
+document.querySelector('#Alex').innerText = display;
---
+++ IWA3/challenge_2/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+import { role1 } from "./nwabisa.js";
+import { role2 } from "./johannes.js";
+import { role3 } from "./alex.js";
+
+console.log('Roles:', role1, role2, role3)
